33851491e27c0240dd0580fafd8612db1a99153c
Richard Turner <rwturner@stanford.edu>
2014-06-12 17:12:39 -0700

    added logging

--- eliza_online.py
+++ eliza_online.py
@@ -9,6 +9,8 @@ import string
 import datetime
 import re
 from nltk import word_tokenize, pos_tag
+import logging
+logging.basicConfig( filename=r'C:\Users\Desktop\Besso\besso_online\elizalog.log', filemode='a' )
 
 def interact(input, rules, default_responses):
     """Have a conversation with a user."""
@@ -28,6 +30,7 @@ def respond(rules, input, default_responses):
             continue
         mgroups = [switch_viewpoint(mg) for mg in m.groups()] # extract and transform free segments
         response = random.choice(rules[rule]).format(*mgroups) # choose and populate response template
+        logging.debug(response)
         return parse_subobj(response.upper())
     return random.choice(default_responses)
 
@@ -69,11 +72,13 @@ def remove_punct(string):
             
 def parse_subobj(input):
     """Correct uses of 'I' that should be 'ME'"""
-    text = nltk.word_tokenize(input)
-    tagset = nltk.pos_tag(text)
+    verbtags = ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    text = word_tokenize(input)
+    tagset = pos_tag(text)
+    logging.debug(tagset)
     output_words = [tagset[0][0]]
     for i in range(1, len(tagset)):
-        if tagset[i][0] == "I" and tagset[i-1][1] == "V":
+        if tagset[i][0] == "I" and tagset[i-1][1] in verbtags:
             output_words.append("ME")
         else:
             output_words.append(tagset[i][0])
